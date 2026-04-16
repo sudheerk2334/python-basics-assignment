b245da8eb93463e2a54cbb0f02dfff4095a08508
sudheerk2334 <54615663+sudheerk2334@users.noreply.github.com>
2026-04-16 21:05:16 +0530

    Create  number_based_reports.py
    https://students.masaischool.com/assignments/76319?tab=assignmentDetails

---
+++ number_based_reports.py
@@ -0,0 +1,47 @@
+# Task 1: Print numbers from 1 to 5
+
+for i in range(1, 6):
+
+  print(i)
+
+
+
+print("-----")
+
+
+
+# Task 2: Print odd numbers between 1 and 10
+
+for i in range(1, 11, 2):
+
+  print(i)
+
+
+
+print("-----")
+
+
+
+# Task 3: Nested loop pattern
+
+for i in range(3):
+
+  for j in range(2):
+
+    print(i, j)
+
+
+
+print("-----")
+
+
+
+# Task 4: Print numbers from 1 to 7, stop at 5
+
+for i in range(1, 8):
+
+  if i == 5:
+
+    break
+
+  print(i)
